ef7fd0fb77be4766dfa7a53b96f1a65d23beb8ae
VicOnt22 <github@selectfix.com>
2023-09-07 09:45:23 -0400

    D10 version with min interface

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -37,25 +37,18 @@ class ComposerAutoloaderInitb333580f8d8ff48e1ebc10601ba10f77
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInitb333580f8d8ff48e1ebc10601ba10f77::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequireb333580f8d8ff48e1ebc10601ba10f77($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitb333580f8d8ff48e1ebc10601ba10f77::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequireb333580f8d8ff48e1ebc10601ba10f77($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
